385e3e04263fcb5e0bd2c5c1915539832c410ee2
= <=>
2025-05-15 18:39:32 +0300

    implement py-cleanup-file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename) -> None:
+        self.filename = filename
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.remove(self.filename)
+
+
+with CleanUpFile("file.txt"):
+    with open("file.txt", "w") as file:
+        file.write("Hello Mate!")
